16f96c12cd5020913e092cc83c5040476f45716f
Patricio Iwanowski <patoiwa@gmail.com>
2020-10-25 12:49:58 -0300

    Problem 6

--- week-4.js
+++ week-4.js
@@ -146,7 +146,7 @@ for (i = 0; i<=4; i++){
     alert(arrayFor[i]);
 }
 //C
-sentence = ""
+sentence = "";
 for (i = 0; i<=4; i++){
     if (i<4){
         sentence += arrayFor[i] + "-";
@@ -154,10 +154,77 @@ for (i = 0; i<=4; i++){
         sentence += arrayFor[i];
     }    
 }
-alert(sentence)
+alert(sentence);
 //D
-arrayEmpty = []
+arrayEmpty = [];
 for (i = 0; i<=9; i++){
     arrayEmpty[i] = i;
 }
-console.log('Arreglo de 10 repeticiones: 'arrayEmpty);
+console.log('Arreglo de 10 repeticiones: ' + arrayEmpty);
+/*
+Funciones
+Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+Crear una función validate integer que reciba un número como parámetro y verdadero si es un número entero.
+A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
+Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
+*/
+console.log('Ejercicio 6');
+//A
+function suma(a,b){
+    return a + b;  
+}
+console.log('Los numeros son 5 y 6');
+a = suma(5,6);
+console.log('La suma es: ' + a);
+//B
+function suma_validate(a,b){
+    if ( (typeof(a) === "number") && typeof(b) === "number"){
+        return a + b;
+    }else{
+        alert("Error: at least one of the parameters was not a number");
+        return NaN;
+    }
+}
+//C
+function validate_integer (a){
+    if( a % 1 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+//D
+function suma_integer_validate(a,b){
+    if ( (typeof(a) === "number") && typeof(b) === "number"){
+        if( a % 1 != 0 ){
+            alert('Error: first number is not an integer');
+            return Math.round(a);
+        }else if ( b % 1 != 0 ){
+            alert('Error: second number is not an integer');
+            return Math.round(b);
+        }else{
+            return a + b;
+        }   
+    }else{
+        alert("Error: at least one of the parameters was not a number");
+        return NaN;
+    }
+}
+//E
+function suma_integer_validate(a,b){
+    if ( (typeof(a) === "number") && typeof(b) === "number"){
+        if(!(validate_integer(a))){
+            alert('Error: first number is not an integer');
+            return Math.round(a);
+        }else if (!(validate_integer(b))){
+            alert('Error: second number is not an integer');
+            return Math.round(b);
+        }else{
+            return a + b;
+        }   
+    }else{
+        alert("Error: at least one of the parameters was not a number");
+        return NaN;
+    }
+}
